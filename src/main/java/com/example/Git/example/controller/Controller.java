5a535a50e6165367ed289c08128ec15a41c2174b
Prasad Chamakuri <53054499+PrasadChamakuri@users.noreply.github.com>
2023-01-29 12:29:57 +0530

    Hello method added

--- src/main/java/com/example/Git/example/controller/Controller.java
+++ src/main/java/com/example/Git/example/controller/Controller.java
@@ -1,7 +1,12 @@
 package com.example.Git.example.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    @GetMapping("/a")
+    public String hello(){
+        return "hello";
+    }
 }
