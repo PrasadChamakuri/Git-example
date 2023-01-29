ed364f029db351db1c10889ddd71fbcb9bbe38e0
Prasad Chamakuri <53054499+PrasadChamakuri@users.noreply.github.com>
2023-01-29 11:42:07 +0530

    Initial commit

---
+++ src/test/java/com/example/Git/example/GitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Git.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
